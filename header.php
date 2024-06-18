9b404aa5d4be1347cb42d6ffe6e663bb92249ae0
Larico <larico@dgtalliance.com>
2024-06-17 21:28:17 -0500

    Update header.php

--- header.php
+++ header.php
@@ -9,6 +9,9 @@
     </title>
    <meta name="viewport" content="width=device-width, initial-scale=1.0" />  
     <!-- <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/images/logo.png" /> -->
+<!--      <link rel="icon" type="image/svg+xml"
+        href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon-light.svg" sizes="128x128"
+        media="(prefers-color-scheme: dark)" /> -->
  
    
     <?php wp_head(); ?>
@@ -18,5 +21,9 @@
 
   <header>
     <h1> <a href="<?php bloginfo('url'); ?>"><?php echo bloginfo(); ?></a> </h1>
-    <?php wp_nav_menu(array( 'theme_location'=> 'topnav', 'menu_class' => '', 'container' => false)); ?>
+    
+<?php wp_nav_menu(array( 'theme_location'=> 'primary', 'menu_class' => 'main-menu', 'container' => false)); ?>
+
+            <?php wp_nav_menu(array( 'theme_location'=> 'top-menu', 'menu_class' => 'header-top-menu', 'container' => false)); ?>
+
   </header>
