b6c6ab1302275daa1a037ebc941296f1920e6437
Larico <chris@larico.dev>
2026-04-08 18:44:59 -0500

    Add contact page template

---
+++ pages/page-contact.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Contact page
+ */
+get_header(); ?>
+
+<style>
+
+</style>
+
+
+<main>
+    <section>
+        <div class="container">
+            <h1>Contact us</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
